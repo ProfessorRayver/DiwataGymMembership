3ec4fd769815109913e0f98a353c766917a13f84
Rayver S. Reyes <121501115+ProfessorRayver@users.noreply.github.com>
2025-03-21 12:46:15 +0800

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -51,7 +51,8 @@ namespace GymSystem
                 Console.WriteLine("\nPlease do select your choices:    (pumili ng ninanais gawin)");
                 Console.WriteLine("[1] View Active Members           (Tignan Ang Mga Aktibong Myembro)");
                 Console.WriteLine("[2] Remove Member                 (Mag tanggal ng Myembro)");
-                Console.WriteLine("[3] Exit");
+                Console.WriteLine("[3] View Workout plans            (Tignan ang mga maaring gawin)");
+                Console.WriteLine("[0] Exit");
 
 
                 Console.Write("Enter Choice:");
@@ -60,10 +61,14 @@ namespace GymSystem
 
                 if (userAction == 1) ViewMem();
                 else if (userAction == 2) RemoveMem();
-                else if (userAction == 3) break;
+                else if (userAction == 3) ViewWorkoutRoutines();
+                else if (userAction == 0) break;
+
                 else Console.WriteLine("ooppss.. Try again. ^_^");
             }
         }
+
+        //methods used in the system.
 
         static void ViewMem() // to view the active list of registered/active membership
         {
@@ -72,8 +77,8 @@ namespace GymSystem
             if (count >= 1) Console.WriteLine("1. " + member1);
             if (count >= 2) Console.WriteLine("2. " + member2);
             if (count >= 3) Console.WriteLine("3. " + member3);
-            if (count >= 4) Console.WriteLine("3. " + member4);
-            if (count >= 6) Console.WriteLine("3. " + member5);
+            if (count >= 4) Console.WriteLine("4. " + member4);
+            if (count >= 5) Console.WriteLine("5. " + member5);
         }
 
         static void RemoveMem() // to remove members who are not continuing to the gym (did not pay on time)
@@ -96,6 +101,19 @@ namespace GymSystem
 
             count--;
             Console.WriteLine("Member Removed!");
+
         }
+
+            static void ViewWorkoutRoutines()
+            {
+                Console.WriteLine("\n|||| Workout Plans ||||");
+                Console.WriteLine("1. Chest Day: Bench Press, Push-ups, Dumbbell Flyes");
+                Console.WriteLine("2. Back Day: Deadlifts, Pull-ups, Lat Pulldowns");
+                Console.WriteLine("3. Leg Day: Squats, Lunges, Leg Press");
+                Console.WriteLine("4. Arm Day: Bicep Curls, Triceps Dips, Hammer Curls");
+                Console.WriteLine("5. Shoulder Day: Overhead Press, Lateral Raises, Face Pulls");
+                Console.WriteLine("6. Cardio: Running, Cycling, Jump Rope");
+            }
+        
     }
 }
